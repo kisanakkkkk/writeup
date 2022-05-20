88a17956760cf3b7261f5e35105feddb4be445aa
kisanakkkkk <70153248+kisanakkkkk@users.noreply.github.com>
2022-05-20 13:41:54 +0700

    Update solve.py

--- Cyber Apocalypse CTF 2022/Jenny From The Block/solve.py
+++ Cyber Apocalypse CTF 2022/Jenny From The Block/solve.py
@@ -9,39 +9,28 @@ import os
 BLOCK_SIZE = 32
 
 def decrypt_block(enc_block, secret):
-    print('enc_bl', enc_block, secret)
-    block = b''
+    ori_block = b''
     for i in range(BLOCK_SIZE):
-        print(enc_block[i])
         val = enc_block[i]
         if val - secret[i] < 0:
-            val+=256
+            val = val + 256
         val = val - secret[i]
-        block += bytes([val])
-        print(bytes([enc_block[i]]), "=>", bytes([val]))
-    return block
+        ori_block += bytes([val])
+    return ori_block
 
 def decrypt(msg):
     msg = binascii.unhexlify(msg)
-    print(msg)
-    if len(msg) % BLOCK_SIZE != 0:
-        msg = pad(msg, BLOCK_SIZE)
-    enc_blocks = [msg[i:i+BLOCK_SIZE] for i in range(0, len(msg), BLOCK_SIZE)]#pemisahan per 32 byte
-    print(enc_blocks)
+    enc_blocks = [msg[i:i+BLOCK_SIZE] for i in range(0, len(msg), BLOCK_SIZE)]
     ct = b''
     h = sha256(enc_blocks[0] + b'Command executed: cat secret.txt').digest()
     for i in range(1, len(enc_blocks)):
-        block = decrypt_block(enc_blocks[i], h)
-        # print('lookatthis', block + enc_bloc)
-        h = sha256(enc_blocks[i] + block).digest()
-        ct += block
-
+        ori_block = decrypt_block(enc_blocks[i], h)
+        h = sha256(enc_blocks[i] + ori_block).digest()
+        ct += ori_block
     return ct
 
-r = remote('209.97.135.38', 30846)
+r = remote('46.101.30.188',31123)
 r.recvuntil('>')
 r.sendline('cat secret.txt')
 encrypted = r.recvline().strip().decode()
 print(decrypt(encrypted))
-
-#b'\nIn case Jenny malfunctions say the following phrase: Melt My Eyez, See Your Future  \nThe AI system will shutdown and you will gain complete control of the spaceship.\n- Danbeer S.A.\nHTB{b451c_b10ck_c1ph3r_15_w34k!!!}\n\x07\x07\x07\x07\x07\x07\x07'
